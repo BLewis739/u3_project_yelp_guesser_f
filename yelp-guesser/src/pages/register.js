97069640af11209cfff248d19c61759169a2dc56
milywendland <milynicwendland@gmail.com>
2022-04-15 12:38:30 -0600

    register user form works

--- yelp-guesser/src/pages/register.js
+++ yelp-guesser/src/pages/register.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { RegisterUser } from '../services/Auth'
 
 const Register = () => {
   let navigate = useNavigate()
@@ -8,56 +9,75 @@ const Register = () => {
     username: '',
     password: '',
     confirmPassword: '',
-    zipcode: '',
+    zipcode: ''
   })
 
-
   const handleChange = (e) => {
-    setFormValues({...formValues, [e.target.name]: e.target.value})
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
   }
-  
 
   const handleSubmit = (e) => {
-   e.preventDefault()
-   await RegisterUser({
-    username: formValues.username,
-    password: formValues.password,
-    zipcode: formValues.zipcode
-   })
-   setFormValues({
-     username: '',
-     password: '',
-     confirmPassword: '',
-     zipcode: ''
-   })
-   navigate('/signin')
+    e.preventDefault()
+    RegisterUser({
+      username: formValues.username,
+      password: formValues.password,
+      zipcode: formValues.zipcode
+    })
+    setFormValues({
+      username: '',
+      password: '',
+      confirmPassword: '',
+      zipcode: ''
+    })
+    navigate('/signin')
   }
 
-
   return (
-    <div className="register">
+    <div>
       <h2>Create an Account</h2>
-      <form className="register-form" onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input onChange={handleChange} type="text" name="username" />
-        </label>
-
-        <label>
-          Password:
-          <input onChange={handleChange} type="text" name="password" />
-        </label>
-
-        <label>
-          Confirm Password:
-          <input onChange={handleChange} type="text" name="confirmPassword" />
-        </label>
-
-        <label>
-          Zipcode:
-          <input onChange={handleChange} type="text" name="zipcode" />
-        </label>
-      </form>
+      <div className="register">
+        <form className="register-form" onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label>
+              Username:
+              <input onChange={handleChange} type="text" name="username" />
+            </label>
+          </div>
+          <div className="input-wrapper">
+            <label>
+              Password:
+              <input onChange={handleChange} type="text" name="password" />
+            </label>
+          </div>
+          <div className="input-wrapper">
+            <label>
+              Confirm Password:
+              <input
+                onChange={handleChange}
+                type="text"
+                name="confirmPassword"
+              />
+            </label>
+          </div>
+          <div className="input-wrapper">
+            <label>
+              Zipcode:
+              <input onChange={handleChange} type="text" name="zipcode" />
+            </label>
+          </div>
+          <div className="button">
+            <button
+              disabled={
+                (!formValues.username && !formValues.zipcode) ||
+                (!formValues.password &&
+                  formValues.confirmPassword === formValues.password)
+              }
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
