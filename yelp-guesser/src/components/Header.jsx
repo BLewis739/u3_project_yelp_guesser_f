eabbc03269e14f436a4c4f98ca44d0284704455d
milywendland <milynicwendland@gmail.com>
2022-04-21 09:16:50 -0600

    added logout functionality

--- yelp-guesser/src/components/Header.jsx
+++ yelp-guesser/src/components/Header.jsx
@@ -12,8 +12,10 @@ import {
 } from '../services/Leaderboard'
 import { useEffect } from 'react'
 
-const Header = (props) => {
 
+const Header = (props) => {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
   const [palette, setPalette] = useState('navBarRed')
 
 
@@ -26,47 +28,110 @@ const Header = (props) => {
     retrieveWorldLeaderboard()
   }, [])
 
+  const handleLogOut = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+    window.location.reload()
+  }
+
+
+
+  let authenticatedOptions
+  if (props.user) {
+    authenticatedOptions = (
+      <nav className={palette}>
+      <div id="navleft-box">
+        <div className="nav">
+          <Link to="/">
+            <img id="nav-logo" src={yelpGuesserLogo} alt="yelp guesser" />
+          </Link>
+        </div>
+        <div className="nav">
+          <Link to="/quickplay">
+            <img id="nav-gamelogo" src={playGameLogo} alt="yelp guesser" />
+          </Link>
+        </div>
+        <div className="nav">
+          <Link to="/scores">
+            <img id="nav-scorelogo" src={leaderBoard} alt="yelp guesser"/>
+          </Link>
+        </div>
+      </div>
+      <div id="navright-box">
+      <div className="nav" id="nav-username">
+          <Link to={"/userpage"}>
+            {'Welcome ' + props.user.username}
+          </Link>
+        </div>
+        <div className='nav' id="logout">
+          <Link to="/" onClick={handleLogOut}>Log Out</Link> 
+        </div>
+        <div className='nav' id="fruit">
+          <div className="accordian horizontal">
+          <img id="strawberry"src={strawberry} alt='strawberry' onClick={()=> setPalette('navBarRed')}></img>
+          <img id="pear" src={pear} alt='pear' onClick={()=> setPalette('navBarGreen')}></img>
+          <img id="orange" src={orange} alt='orange' onClick={()=> setPalette('navBarOrange')}></img>
+          </div>
+        </div>
+        
+        <div className="nav">
+          <Link to="/About">
+            <img id="nav-aboutlogo" src={questionMark} alt="yelp guesser" />
+          </Link>
+        </div>
+      </div>
+    </nav>
+    )
+  }
+
+  const publicOptions = (
+    <nav className={palette}>
+    <div id="navleft-box">
+      <div className="nav">
+        <Link to="/">
+          <img id="nav-logo" src={yelpGuesserLogo} alt="yelp guesser" />
+        </Link>
+      </div>
+      <div className="nav">
+        <Link to="/quickplay">
+          <img id="nav-gamelogo" src={playGameLogo} alt="yelp guesser" />
+        </Link>
+      </div>
+      <div className="nav">
+        <Link to="/scores">
+          <img id="nav-scorelogo" src={leaderBoard} alt="yelp guesser"/>
+        </Link>
+      </div>
+    </div>
+    <div id="navright-box">
+    <div className="nav" id="nav-username">
+        <Link to={'/login'}>
+          {'sign in'}
+        </Link>
+      </div>
+      <div className='nav' id="fruit">
+        <div className="accordian horizontal">
+        <img id="strawberry"src={strawberry} alt='strawberry' onClick={()=> setPalette('navBarRed')}></img>
+        <img id="pear" src={pear} alt='pear' onClick={()=> setPalette('navBarGreen')}></img>
+        <img id="orange" src={orange} alt='orange' onClick={()=> setPalette('navBarOrange')}></img>
+        </div>
+      </div>
+      
+      <div className="nav">
+        <Link to="/About">
+          <img id="nav-aboutlogo" src={questionMark} alt="yelp guesser" />
+        </Link>
+      </div>
+    </div>
+  </nav>
+  )
 
   return (
     <header>
-      <nav className={palette}>
-        <div id="navleft-box">
-          <div className="nav">
-            <Link to="/">
-              <img id="nav-logo" src={yelpGuesserLogo} alt="yelp guesser" />
-            </Link>
-          </div>
-          <div className="nav">
-            <Link to="/quickplay">
-              <img id="nav-gamelogo" src={playGameLogo} alt="yelp guesser" />
-            </Link>
-          </div>
-          <div className="nav">
-            <Link to="/scores">
-              <img id="nav-scorelogo" src={leaderBoard} alt="yelp guesser"/>
-            </Link>
-          </div>
-        </div>
-        <div id="navright-box">
-          <div className='nav' id="fruit">
-            <div className="accordian horizontal">
-            <img id="strawberry"src={strawberry} alt='strawberry' onClick={()=> setPalette('navBarRed')}></img>
-            <img id="pear" src={pear} alt='pear' onClick={()=> setPalette('navBarGreen')}></img>
-            <img id="orange" src={orange} alt='orange' onClick={()=> setPalette('navBarOrange')}></img>
-            </div>
-          </div>
-          <div className="nav" id="nav-username">
-            <Link to={props.user  ? "/userpage" : '/login'}>
-              {props.user ? 'Welcome ' + props.user.username : 'sign in'}
-            </Link>
-          </div>
-          <div className="nav">
-            <Link to="/About">
-              <img id="nav-aboutlogo" src={questionMark} alt="yelp guesser" />
-            </Link>
-          </div>
-        </div>
-      </nav>
+    
+      {props.authenticated && props.user ? authenticatedOptions : publicOptions}
+ 
     </header>
   )
 }
