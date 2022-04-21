ee137848db15603d0f89db7879ce5eacb930a302
milywendland <milynicwendland@gmail.com>
2022-04-21 14:51:14 -0600

    added catguesser page, added logo to page, click on crying cat, brings to page

--- yelp-guesser/src/pages/home.js
+++ yelp-guesser/src/pages/home.js
@@ -68,7 +68,13 @@ const Home = () => {
       </div>
 
       <img id="hot-guess-img" src={hotGuessLogo} alt="hot guesses" />
-      <img id="cry-cat" src={cryCat} />
+      <img
+        id="cry-cat"
+        src={cryCat}
+        onClick={() => {
+          navigate('/catguesser')
+        }}
+      />
     </div>
   )
 }
